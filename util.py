bb9b77b4482b78d85b152dd0cf8f9cfb5c67eb67
Reza <reza.irvanda@xtremax.com>
2017-10-25 08:50:02 +0700

    Fixing draw_vector

--- util.py
+++ util.py
@@ -6,8 +6,8 @@ def draw_vector(v0, v1, ax=None):
     Draw arrow-style vector based on v0 and v1
     """
     ax = ax or plt.gca()
-    arrowprops = dict(arrowstyle='-->', linewidth=2, shrinkA=0, shrinkB=0)
-    ax.annonate('', v1, v0, arrowprops=arrowprops)
+    arrowprops = dict(arrowstyle='->', linewidth=2, shrinkA=0, shrinkB=0)
+    ax.annotate('', v1, v0, arrowprops=arrowprops)
     plt.show()
 
 def plot_digits(data):
